9f52b9c9cbd1074e6df94e0318b69810d48edb79
Nicolas Perozzi <perozzinicolas@gmail.com>
2025-11-04 22:09:35 +0100

    Added draw_snake().

--- main.c
+++ main.c
@@ -11,14 +11,30 @@ EXERCISE:
 #define SIZE_Y 20
 #define SIZE_X 20
 
+typedef struct
+{
+    int seg_x;
+    int seg_y;
+} snake_seg_t;
+
+typedef struct
+{
+    snake_seg_t seg_array[900];
+    int snake_size;
+    int direction_y;        //-1 = left, 1 = right, 0 = no movement in that axis.
+    int direction_x;        //-1 = up, 1 = down, 0 = no movement in that axis
+} snake_t;
+
 
 char display[SIZE_Y][SIZE_X];
 
 void update_display(snake_t *snake, food_t *food);
 void draw_field(char ch);
+void draw_snake(char ch, snake_t *snake);
 
 int main(void)
 {
+    snake_t snake;
         update_display(&snake, &food);
     return 0;
 }
@@ -38,9 +54,18 @@ void draw_field(char ch)
         }
     }
 }
+
+void draw_snake(char ch, snake_t *snake)
+{
+    for(int i = 0; i < snake->snake_size; i++)
+    {
+        display[snake->seg_array[i].seg_y][snake->seg_array[i].seg_x] = ch;
+    }
+}
 void update_display(snake_t *snake, food_t *food)
 {
     draw_field('#');
+    draw_snake('@', snake);
 
     for(int y = 0; y < SIZE_Y; y++)
     {
